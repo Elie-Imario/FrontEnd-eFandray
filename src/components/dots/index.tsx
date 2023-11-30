1c46d33bd46eae15d825b40d53eca2882c2cdc93
Imarioa <elieimario1@gmail.com>
2023-11-30 16:50:52 +0300

    create Dots component

---
+++ src/components/dots/index.tsx
@@ -0,0 +1,14 @@
+import './dots.style.scss'
+
+const Dots = () => {
+  return (
+    <div className="dots-statement">
+        <span className="dots single"></span>
+        <span className="dots single1"></span>
+        <span className="dots single2"></span>
+    </div>
+  )
+}
+
+
+export default Dots;
--- src/components/userCard/ProfilPic.tsx
+++ src/components/userCard/ProfilPic.tsx
@@ -12,13 +12,6 @@ const ProfilPic: FC<Props> = ({_profilPicPath, _width, _height}) => {
     <div className="profil-pic" style={{width: _width, height:_height}}>
         <img src={_profilPicPath}/>
         <span className="online-dots"></span>
-        {/* <p>
-            <span className="dots-statement">
-                <span className="dots single"></span>
-                <span className="dots single1"></span>
-                <span className="dots single2"></span>
-            </span>
-        </p> */}
     </div>
   )
 }
