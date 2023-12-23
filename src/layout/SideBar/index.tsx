d0313f2b1c202f505c773849a10d7f6873f618de
Imarioa <elieimario1@gmail.com>
2023-12-23 15:21:56 +0300

    setUp Logout action

--- src/layout/SideBar/index.tsx
+++ src/layout/SideBar/index.tsx
@@ -6,12 +6,17 @@ import ProfilPic from "../../components/userCard/ProfilPic"
 import './sideBarStyle.scss'
 
 const SideBar = () => {
-    const { UserLogContext } = useContext(AppContext)
+    const { UserLogContext, setAppContext } = useContext(AppContext)
+    const setUpLogout = ()=>{
+      sessionStorage.clear()
+      setAppContext(undefined)
+    }
+
     return (
       <div className="SideBar-section">
           <div className="sideBar-bottom-section">
-            <ProfilPic _isOnline={UserLogContext?.status as boolean} _height={54} _width={54} _profilPicPath={UserLogContext?.profilpic_path}/>
-            <button className='btn btn-settings'><FontAwesomeIcon icon="sign-out-alt" size="lg" /></button>
+            <ProfilPic _isOnline={UserLogContext?.status as boolean} _height={45} _width={45} _profilPicPath={UserLogContext?.profilpic_path}/>
+            <button className='btn btn-settings' onClick={setUpLogout}><FontAwesomeIcon icon="sign-out-alt" size="lg" /></button>
           </div>
       </div>
     )
